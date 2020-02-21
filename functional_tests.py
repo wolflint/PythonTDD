0035f9cb191e0a74f9bd4f85ff035cd022fd6868
Marcin Wolf <wolflint@protonmail.com>
2020-02-21 17:25:36 +0000

    Added a helper function for checking list table contents

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Testing the home page of the to-do app
         self.browser.get('http://localhost:8000')
@@ -43,8 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy cheese', [row.text for row in rows])
-        self.assertIn('2: Buy ham', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy cheese')
+        self.check_for_row_in_list_table('2: Buy ham')
 
         # There is still a text box inviting her to add another item. She enters
         # "Use cheese to make a sandwich"
