f77686a4b476a199ef3dd8b7edb9bd7e8c599238
Marcin Wolf <wolflint@protonmail.com>
2020-03-10 18:02:26 +0000

    modified views, urls and moved code + tests

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,11 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()
