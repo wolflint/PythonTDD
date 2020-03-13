cfcff10c8925bbd38f395a52c45f65b94330bae8
Marcin Wolf <wolflint@protonmail.com>
2020-03-13 14:15:24 +0000

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -99,7 +99,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Again, there is not trace of User1's list
         page_text = self.browser.find_element_by_tag_name('body').text
-        self.assetNotIn('Buy tennis racket', page_text)
+        self.assertNotIn('Buy tennis racket', page_text)
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, both users go to sleep ZzzzZzzzzZzzz
@@ -128,3 +128,27 @@ class NewVisitorTest(LiveServerTestCase):
         # Satisfied, the user goes to sleep
 # if __name__ == '__main__':
 #     unittest.main(warnings='ignore')
+    def test_layout_and_styling(self):
+        # User goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # The user notices that the input box is nicesly centred
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # The user starts a new list and sees that the input is nicely
+        # centred there too.
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+        inputbox.location['x'] + inputbox.size['width'] / 2,
+        512,
+        delta=10
+        )
